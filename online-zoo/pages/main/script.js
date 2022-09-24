aa36a01b1b9a71326bf28251bc0e35f4f37405a5
Angelina <zhuravskayalina@gmail.com>
2022-09-24 11:07:55 +0400

    feat: added scrollbar animation in testimonials section

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,3 +1,5 @@
+// email input section
+
 const emailInput = document.querySelector('.input-email');
 const submitButton = document.querySelector('.input-submit');
 
@@ -21,4 +23,39 @@ function onInput(event) {
         emailInput.classList.add('mistake-email');
     }
 };
+
+
+// scrollbar
+
+const scroll = document.querySelector('.scroll');
+const scrollContainer = document.querySelector('.scroll-bar');
+
+let dragging = false; //if scroll dragging rn
+let startX = 0; //start coords
+let startY = 0;
+
+scroll.addEventListener('mousedown', function (event) {
+    dragging = true;
+    scroll.style.backgroundColor = '#4B9200';
+    startX = event.pageX - Number.parseInt(scroll.style.left || 0);
+    startY = event.pageY - Number.parseInt(scroll.style.top || 0);
+
+});
+
+document.body.addEventListener('mousemove', (event) => {
+    if (!dragging) return;
+    let newPositionX = event.pageX - startX;
+    scroll.style.left = `${newPositionX}px`;
+
+    if (newPositionX <= 2) {
+        scroll.style.left = 2 + 'px'
+    } else if (newPositionX >= 482) {
+        scroll.style.left = 482 + 'px'
+    };
+})
+
+document.body.addEventListener('mouseup', () => {
+    dragging = false;
+    scroll.style.backgroundColor = '#F9804B';
+});
 
