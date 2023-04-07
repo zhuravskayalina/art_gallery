d1541a24ca27cbce10657241b619b4d36247cda7
Angelina <zhuravskayalina@gmail.com>
2023-04-07 16:56:00 +0200

    feat: add styles to searchbar

--- react-ts-app/src/components/Exhibition/RickMorty/RickAndMorty.tsx
+++ react-ts-app/src/components/Exhibition/RickMorty/RickAndMorty.tsx
@@ -8,21 +8,30 @@ const RickAndMortyExhibition = () => {
   return (
     <div className={cx('container')}>
       <div className={cx('text')}>
-        <h2 className={cx('text__heading')}>Welcome to our Rick and Morty Exhibition!</h2>
+        <h2 className={cx('text__heading')}>Welcome to our Rick and Morty
+          Exhibition!</h2>
         <h3 className={cx('text__subheading')}>
-          Calling all Rick & Morty fans! A free exhibition comes to Paris. From March 10 to 20,
-          2023, go and discover works by ten artists revisiting the main pop culture characters
+          Calling all Rick & Morty fans! A free exhibition comes to Paris. From
+          March 10 to 20,
+          2023, go and discover works by ten artists revisiting the main pop
+          culture characters
           during this temporary event.
         </h3>
         <p className={cx('text__description')}>
-          Rick & Morty is a crazy, dark humored series taking us to a fun sci-fi universe. Now fully
-          part of pop-culture, it takes us to the adventures of Morty Smith and his grandfather,
-          Rick Sanchez, through infinite dimensions. As the series – making fun of Back to the
-          Future – is wrapping the 6th season, a temporary event in Paris is likely to make you very
+          Rick & Morty is a crazy, dark humored series taking us to a fun sci-fi
+          universe. Now fully
+          part of pop-culture, it takes us to the adventures of Morty Smith and
+          his grandfather,
+          Rick Sanchez, through infinite dimensions. As the series – making fun
+          of Back to the
+          Future – is wrapping the 6th season, a temporary event in Paris is
+          likely to make you very
           happy.
         </p>
       </div>
-      <SearchBar />
+      <div className={cx('searchbar')}>
+        <SearchBar/>
+      </div>
     </div>
   );
 };
